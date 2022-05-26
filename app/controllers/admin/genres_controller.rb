58f3e090244072f096ab171e2bda9c29b178128c
ishizuya <102888150+ishizuya@users.noreply.github.com>
2022-05-26 14:21:10 +0000

    [Add]ジャンル編集機能

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -16,17 +16,18 @@ class Admin::GenresController < ApplicationController
 
   def edit
     @genre = Genre.find(params[:id])
-
   end
 
   def update
-
+    @genre = Genre.find(params[:id])
+    @genre.update(genre_params)
+    redirect_to admin_genres_path
   end
 
   private
 
   def genre_params
-    params.permit(:name)
+    params.require(:genre).permit(:name)
   end
 
 end
